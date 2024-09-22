aedf2132755550bc5e98d7a3bb61ff86a92a220a
Wes Nicol <38409659+wesnicol@users.noreply.github.com>
2024-09-22 09:41:59 -0700

    Inital commit of stat prediction. Still some bugs that need fixing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 from pprint import pprint
 from odds_api import load_player_odds
+from predicted_stats import predict_stats_for_player
 
 
 # Example usage of loading saved odds
@@ -9,5 +10,5 @@ player_names = ["Joe Burrow", "Drake London", "Zach Charbonnet", "Sam LaPorta"]
 
 for player_name in player_names:
     if player_name in all_player_odds.keys():
-        print(player_name + " Odds:")
-        pprint(all_player_odds[player_name])
+        print(player_name + " Projected Stats:")
+        pprint(predict_stats_for_player(all_player_odds[player_name]))
